18a1c9f4186fcfd34fc711a513d75739b2fafbc0
GabrielGLuna <gabrielunagarcia@hotmail.com>
2023-10-09 09:56:41 -0600

    Rick and Morty pt1

---
+++ RickandMorty/app.js
@@ -0,0 +1,50 @@
+const URL = 'https://rickandmortyapi.com/api';
+const containerChar = document.querySelector('.characters');
+
+fetch(`${ URL }/character`)
+.then(Response => Response.json())
+.then(data => {
+    console.log(data.info);
+    const characters = data.results;
+    /*characters.forEach(character => {
+        const p = document.createElement('p');
+        p.innerHTML = character.name;
+        containerChar.appendChild(p);
+    })*/
+    showCharacters(characters);
+})
+
+/*
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+*/
+function createCard(character){
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.style.width = '18rem';
+    const htmlCard = `
+    <img src="${ character.image }" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${ character.name }</h5>
+      <p class="card-text">${ character.status }</p>
+      <p class="card-text">${ character.origin.name }</p>
+      <a href="#" class="btn btn-primary" data-id="${ character.id }">Go somewhere</a>
+      </div>
+      `
+      card.innerHTML = htmlCard;
+      return card;
+    
+}
+function showCharacters(characters){
+    containerChar.innerHTML = '';
+    characters.forEach(character => {
+        containerChar.appendChild(createCard(character));
+    });
+}
+
