7497a341754633ed3ffec844eb94e882dae292a2
GabrielGLuna <gabrielunagarcia@hotmail.com>
2023-10-10 12:40:25 -0600

    Rick and MOrty API parte 2

--- RickandMorty/app.js
+++ RickandMorty/app.js
@@ -1,7 +1,24 @@
 const URL = 'https://rickandmortyapi.com/api';
 const containerChar = document.querySelector('.characters');
+const pagination = document.querySelector('.pagination');
 
-fetch(`${ URL }/character`)
+function createPage(){
+  let buttons = ``;
+  for(let i = 1; i<=42; i++){
+    buttons += `
+    <li class="page-item">
+    <a class="page-link" href="#" data-id="${i}">${i}</a>
+    </li>
+    `
+  }
+  pagination.innerHTML = buttons;
+}
+
+//const page = prompt('Cual página? ')
+createPage();
+
+function getCharacter(page=1){
+fetch(`${ URL }/character/?page=${page}`)
 .then(Response => Response.json())
 .then(data => {
     console.log(data.info);
@@ -13,6 +30,11 @@ fetch(`${ URL }/character`)
     })*/
     showCharacters(characters);
 })
+}
+
+getCharacter();
+
+
 
 /*
 <div class="card" style="width: 18rem;">
@@ -26,7 +48,7 @@ fetch(`${ URL }/character`)
 */
 function createCard(character){
     const card = document.createElement('div');
-    card.classList.add('card');
+    card.classList.add('card', 'mt-3', 'bg-secondary-subtitle');
     card.style.width = '18rem';
     const htmlCard = `
     <img src="${ character.image }" class="card-img-top" alt="...">
@@ -34,7 +56,7 @@ function createCard(character){
       <h5 class="card-title">${ character.name }</h5>
       <p class="card-text">${ character.status }</p>
       <p class="card-text">${ character.origin.name }</p>
-      <a href="#" class="btn btn-primary" data-id="${ character.id }">Go somewhere</a>
+      <a href="#" class="btn btn-success" data-id="${ character.id }">  Ver mas</a>
       </div>
       `
       card.innerHTML = htmlCard;
@@ -47,4 +69,13 @@ function showCharacters(characters){
         containerChar.appendChild(createCard(character));
     });
 }
+
+function getButton(e){
+  e.preventDefault();
+  if(e.target.classList.contains('page-link')){
+    id = e.target.getAttribute('data-id');
+    getCharacter(id);
+  }
+}
+pagination.addEventListener('click', getButton);
 
